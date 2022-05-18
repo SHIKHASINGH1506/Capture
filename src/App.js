d8cda8989d65d29f78c5b8ac23a3e5442cdcfef4
Shikha <shikhakumarisingh9715@gmail.com>
2022-05-18 22:01:19 +0530

    get all users when app starts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import "./App.css";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { Navbar, Container } from 'component';
-import { Login, Signup } from 'features';
+import { authState, Login, Signup, getAllUser } from 'features';
 import { Home, Bookmark, UserProfile } from 'views';
 import { ProtectedRoute } from "routes/ProtectedRoute";
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const { token } = useSelector(authState);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (token) {
+      (async () => {
+        try {
+          const response = await dispatch(getAllUser());
+          if (response?.error)
+            throw new Error('Error in fetching users');
+        } catch (error) {
+          console.log(error.message);
+        }
+      })()
+    }
+  },
+  [token]);
+
   return (
     <div className='max-w-screen-xl m-auto'>
       <ToastContainer
@@ -19,32 +39,30 @@ function App() {
         rtl={false}
         draggable
         pauseOnHover
-
       />
       <Navbar />
       <Routes>
         <Route path='/' element={
           <ProtectedRoute>
             <Container>
-              <Home/>
+              <Home />
             </Container>
           </ProtectedRoute>}>
         </Route>
-         <Route path='/bookmark' element={
+        <Route path='/bookmark' element={
           <ProtectedRoute>
             <Container>
-              <Bookmark/>
+              <Bookmark />
             </Container>
           </ProtectedRoute>}>
         </Route>
         <Route path='/profile' element={
           <ProtectedRoute>
             <Container>
-              <UserProfile/>
+              <UserProfile />
             </Container>
           </ProtectedRoute>}>
         </Route>
-
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
